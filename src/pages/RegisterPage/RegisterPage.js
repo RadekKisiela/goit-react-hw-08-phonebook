45974e98cc40559808236a9b1b3a02aab88fc52e
RadekKisiela <radek1997_97@o2.pl>
2024-01-20 17:28:21 +0100

    update

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RegisterForm from 'components/RegisterForm/RegisterForm';
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 
 export const RegisterPage = () => {
   return (
