9e0ae9f86f964f58ccd7a724e169777f69d928d0
RadekKisiela <radek1997_97@o2.pl>
2024-01-26 23:18:42 +0100

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/docs/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 const setHeader = token => {
   axios.defaults.headers.common.Authorization = token ? `Bearer ${token}` : '';
